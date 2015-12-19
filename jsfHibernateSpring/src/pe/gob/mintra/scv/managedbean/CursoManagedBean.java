ae1f92c2cd3ecb2bd0d01c6bae43e57ccbbfba1c
Luis Benvenuto <lbenvenutoc@gmail.com>
2015-12-18 20:31:52 -0500

    Se agrego y vinculo archivo para css y carpeta de iconos para imagenes de los  botones

--- jsfHibernateSpring/src/pe/gob/mintra/scv/managedbean/CursoManagedBean.java
+++ jsfHibernateSpring/src/pe/gob/mintra/scv/managedbean/CursoManagedBean.java
@@ -1,34 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package pe.gob.mintra.scv.managedbean;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.logging.Level;
-
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.faces.model.SelectItem;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.Priority;
-import org.primefaces.context.RequestContext;
+import org.primefaces.model.UploadedFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
 import pe.gob.mintra.scv.model.Curso;
-import pe.gob.mintra.scv.model.Pregunta;
 import pe.gob.mintra.scv.model.UnidadAprendizaje;
 import pe.gob.mintra.scv.service.CursoService;
-import pe.gob.mintra.scv.util.Constante;
 
 /**
  * 
@@ -52,6 +37,8 @@ public class CursoManagedBean implements Serializable {
 	private List<UnidadAprendizaje> lstUnidad;
 
 	public boolean muestraUnidad = false;
+
+	private UploadedFile archivoSilabo;
 
 	public CursoManagedBean() {
 		inicializarCurso();
@@ -106,6 +93,8 @@ public class CursoManagedBean implements Serializable {
 			cursoService.actualizarCurso(objCurso, outParametersCurso);
 
 		}
+
+		
 
 		muestraUnidad = true;
 
@@ -236,6 +225,14 @@ public class CursoManagedBean implements Serializable {
 
 	public void setMuestraUnidad(boolean muestraUnidad) {
 		this.muestraUnidad = muestraUnidad;
+	}
+
+	public UploadedFile getArchivoSilabo() {
+		return archivoSilabo;
+	}
+
+	public void setArchivoSilabo(UploadedFile archivoSilabo) {
+		this.archivoSilabo = archivoSilabo;
 	}
 
 }
